f5275a115463e4a55d5f86db80044dd68b3c6482
timelyportfolio <kent.russell@timelyportfolio.com>
2013-10-17 10:53:49 -0500

    various examples of rCharts dataTable usage
    this first commit has a custom sorting demonstration

---
+++ customsort.R
@@ -0,0 +1,16 @@
+library(gdata)
+library(rCharts)
+library(knitr)
+v1=as.factor(rep(c("level1","level2"),50))
+v1=as.data.frame(v1)
+v2=as.factor(rep(c("levelA","levelB","levelC"),100))
+v2=as.data.frame(v2)             
+v1v2=cbindX(v1,v2)
+tab=dTable(v1v2, sPaginationType = "full_numbers")
+tab$setLib("./datatables")
+tab$params$table$aoColumns =
+    list(
+      list(sType = "string_ignore_null", sTitle = "v1"),
+      list(sTitle = "v2")
+    )
+tab
